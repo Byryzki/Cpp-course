b0c2607322008f759164d5e9e5d4c9a053b4380c
Pyry Laine <pyry.j.laine@tuni.fi>
2021-01-15 16:32:43 +0200

    lämpötila

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -11,8 +11,8 @@ int main()
     double cel = (temp - 32) / 1.8;
     double far = temp * 1.8 +32;
 
-    cout << temp << " degrees Celsius is " << far << " degrees in Fahrenheit" << endl;
-    cout << temp << " degrees Fahrenheit is " << cel << " degrees in Celsius" << endl;
+    cout << temp << " degrees Celsius is " << far << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << cel << " degrees Celsius" << endl;
 
     return 0;
 }
