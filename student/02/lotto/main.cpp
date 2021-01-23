73f4b86c35263f2e1c2e0804a23b92955204e5e1
Pyry Laine <pyry.j.laine@tuni.fi>
2021-01-23 17:59:51 +0200

    lotto

---
+++ student/02/lotto/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int sihti(int balls, int drawn){
+    // lukujen tarkastus
+
+    if(balls < 0){
+        cout << "The number of balls must be a positive number." << endl;
+        return false;
+    }
+    else{
+        if(balls < drawn){
+            cout << "The maximum number of drawn balls is the total amount of balls." << endl;
+            return false;
+        }
+        else{
+            return balls, drawn;
+        }
+    }
+}
+
+int laskuri(int balls, int drawn){
+    unsigned long int result = 0;
+    unsigned long long fact = 1;
+    unsigned long long fact2 = 1;
+    unsigned long long fact3 = 1;
+
+    for(int i = 1; i<= balls; ++i){
+        fact *= i;
+    }
+    for(int i = 1; i<= drawn; ++i){
+        fact2 *= i;
+    }
+    for(int i = 1; i<= balls-drawn; ++i){
+        fact3 *= i;
+    }
+    result = fact / (fact3 * fact2);
+
+    return result;
+}
+
+int main()
+{
+    cout << "Enter a total number of lottery balls: ";
+    int balls = 0;
+    cin >> balls;
+
+    cout << "Enter the number of drawn balls: ";
+    int drawn = 0;
+    cin >> drawn;
+
+    if(sihti(balls, drawn)){
+        cout << "The probability of guessing all " << drawn << " balls correctly is " << "1/" << laskuri(balls, drawn) << endl;
+    }
+    else{
+
+    }
+
+
+    return 0;
+}
