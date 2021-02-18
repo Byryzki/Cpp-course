26bed8e500e3bb2f6c975ebd3f892b7cad704499
Pyry Laine <pyry.j.laine@tuni.fi>
2021-02-18 22:20:26 +0200

    wordcount ilman tulostus

--- student/05/wordcount/main.cpp
+++ student/05/wordcount/main.cpp
@@ -24,15 +24,16 @@ int main()
     }
     else
     {
+        map<map<string, int>, map<string, vector<int>>> valmis;
+        map<string, vector<int>> rivit;
         map<string, int> montako;
-        vector<int> rivit;
         std::string rivi;
         int rivinro = 1;
 
         while(getline(inf, rivi)) //rivi rekursio
         {
             size_t pos = 0;
-            while((pos = rivi.find(" ")) != string::npos) // viimeinen sana puuttuu
+            while((pos = rivi.find(" ")) != string::npos)
             {
                 string sana = rivi.substr(0, pos);
                 rivi.erase(0, pos+1);
@@ -40,35 +41,43 @@ int main()
                 if(montako.find(sana) != montako.end())
                 {
                     montako.at(sana) += 1;
+                    rivit[sana].push_back(rivinro);
                 }
                 else
                 {
                     montako.insert({sana, 1});
-
+                    rivit[sana].push_back(rivinro);
                 }
             }
 
             if(montako.find(rivi) != montako.end())
             {
                 montako.at(rivi) += 1;
+                rivit[rivi].push_back(rivinro);
             }
             else
             {
                 montako.insert({rivi, 1});
-
+                rivit[rivi].push_back(rivinro);
             }
 
             rivinro += 1;
         }
+
         inf.close();
 
         map<string, int>::iterator iter = montako.begin();
+
+        map<string, vector<int>>::iterator iter2 = rivit.begin();
+        int i = 0;
         while(iter != montako.end())
         {
-            cout << iter-> first << ": " << iter-> second << endl;
-            ++iter;
+            cout << iter-> first << ": " << iter-> second;
+            cout << (iter2 -> second).at(i);
         }
-    }
+        ++iter;
+        i += 1; // tee tulostus
+        }
 
-    return 0;
+   return 0;
 }
