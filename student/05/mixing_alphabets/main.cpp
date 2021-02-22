9912a0f3275644386d84158d05af7b48c50f79bf
Pyry Laine <pyry.j.laine@tuni.fi>
2021-02-22 20:48:19 +0200

    mixed alphabet korjaus

--- student/05/mixing_alphabets/main.cpp
+++ student/05/mixing_alphabets/main.cpp
@@ -11,26 +11,21 @@ int main()
 
     std::cout << "Enter some text. Quit by entering the word \"END\"." << std::endl;
     std::string word;
-    std::vector <std::string> kumma;
 
     while (std::cin >> word)
     {
         if (word == "END")
         {
-            std::vector<std::string>::iterator lapi = kumma.begin();
-
-            while(lapi != kumma.end())
-            {
-                std::cout << *lapi << std::endl;
-                ++lapi;
-            }
-
             return EXIT_SUCCESS;
         }
 
         // TODO: implement your solution here
-        shuffle(++word.begin(), --word.end(), generator);
-        kumma.push_back(word);
+        if(word.length() > 2)
+        {
+            std::shuffle(word.begin() +1, word.end() -1, generator);
+        }
+        std::cout << word << std::endl;
     }
 
+    return EXIT_SUCCESS;
 }
