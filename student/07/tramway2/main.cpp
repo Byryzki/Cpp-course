32ea5b2989f9d16a36d461bcf2f2b1e28810add0
Pyry Laine <pyry.j.laine@tuni.fi>
2021-03-05 18:21:34 +0200

    RASSE -projekti: LINE, STOPS, STOP

--- student/07/tramway2/main.cpp
+++ student/07/tramway2/main.cpp
@@ -54,83 +54,184 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     return result;
 }
 
-int LINE(std::map<std::string, std::map<std::string, double>> routes, std::string line)
+bool double_compare(std::pair<std::string, double>& a, std::pair<std::string, double>& b)
 {
-    for(std::map<std::string, double>::iterator i = routes[line].begin(); i != routes[line].end(); i++) // lisää loop alamapille.
-    {
-        std::cout << " - " << i -> first << " : " << i -> second << std::endl;
-    }
-
-    return 0;
+    return a.second < b.second;
 }
 
-int LINES(std::map<std::string, std::map<std::string, double>> routes)
+bool str_compare(std::string a, std::string b)
 {
-    for(std::map<std::string, std::map<std::string, double>>::iterator i = routes.begin(); i != routes.end(); i++)
+    return a < b;
+}
+
+std::vector<std::pair<std::string, double>> value_sort(std::map<std::string, double> line)
+{
+    /* Luo listan linjan pysäkeistä etäisyysjärjestyksessä */
+
+    std::vector<std::pair<std::string, double>> sorted;
+    for(auto& stop : line)
     {
-        std::cout << i -> first << std::endl;
+        sorted.push_back(stop);
     }
 
-    return 0;
+    sort(sorted.begin(), sorted.end(), double_compare);
+
+    return sorted;
 }
+
+void LINE(std::map<std::string, std::map<std::string, double>> routes, std::string line)
+{   
+    std::vector<std::pair<std::string, double>> sorted = value_sort(routes[line]);
+
+    for(auto stop : sorted)
+    {
+        std::cout << " - " << stop.first << " : " << stop.second << std::endl;
+    }
+}
+
+void LINES(std::map<std::string, std::map<std::string, double>> routes)
+{
+    for(std::map<std::string, std::map<std::string, double>>::iterator line = routes.begin(); line != routes.end(); line++)
+    {
+        std::cout << line -> first << std::endl;
+    }
+}
+
+void STOPS(std::map<std::string, std::map<std::string, double>> routes)
+{
+    std::vector<std::string> submap;
+
+    for(std::map<std::string, std::map<std::string, double>>::iterator line = routes.begin(); line != routes.end(); line++)
+    {
+        for(std::map<std::string, double>::iterator stop = routes[line -> first].begin(); stop != routes[line -> first].end(); stop++)
+        {
+            submap.push_back(stop -> first);
+        }
+    }
+
+    // aakkosjärjestys ja toisto pois
+    sort(submap.begin(), submap.end(), str_compare);
+    std::vector<std::string>::iterator stops = std::unique(submap.begin(), submap.end());
+    submap.resize(std::distance(submap.begin(), stops));
+
+    for(stops = submap.begin(); stops != submap.end(); ++stops)
+    {
+        std::cout << *stops << std::endl;
+    }
+}
+
+void STOP(std::map<std::string, std::map<std::string, double>> routes, std::string target)
+{
+    std::vector<std::string> lines;
+
+    for(std::map<std::string, std::map<std::string, double>>::iterator line = routes.begin(); line != routes.end(); line++)
+    {
+        for(std::map<std::string, double>::iterator stop = routes[line -> first].begin(); stop != routes[line -> first].end(); stop++)
+        {
+            if(stop -> first == target)
+            {
+                std::cout << " - " << line -> first << std::endl;
+            }
+        }
+    }
+}
+
+/* Jos jaksaminen riittää, korjaa lainausmerkkejä varten
+std::vector<std::string> command_repair(std::string command, char del)
+{
+    // etsii komennosta paikannimen lainausmerkkien perusteella ja palauttaa paikan nimen
+
+    std::vector<std::string> cmd;
+    bool quote = false;
+
+    for(auto& mark : command)
+    {
+        if(mark == del)
+        {
+            quote = true;
+            break;
+        }
+    }
+
+    cmd = split(command, ' ', true);
+    if(quote)
+    {
+        std::string name;
+        if(split(command, ' ', true).size() == 3)
+        {
+            command.erase(std::remove(command.begin(), command.end(), del), command.end());
+            name = cmd.at(1) + " " + cmd.at(2);
+            cmd.at(1) = name;
+            cmd.resize(2);
+        }
+    }
+
+    return cmd;
+}
+*/
 
 int UI(std::map<std::string, std::map<std::string, double>> routes)
 {
-    while(true)
+    if(routes.size() > 0)
     {
-        std::cout << "tramway> ";
-        std::string command;
-        getline(std::cin, command);
-
-        std::vector<std::string> cmd = split(command, ' ', true);
-
-        /* cmd.at(0): komento, cmd.at(1): linja tai pysäkki yms. */
-        if(cmd.at(0) == "LINES")
+        while(true)
         {
-            std::cout << "All tramlines in alphabetical order:" << std::endl;
-            LINES(routes);
+            std::cout << "tramway> ";
+            std::string command;
+            getline(std::cin, command);
+
+            std::vector<std::string> cmd = split(command, ' ', true);
+
+            /* cmd.at(0): komento, cmd.at(1): linja tai pysäkki yms. */
+            if(cmd.at(0) == "LINES")
+            {
+                std::cout << "All tramlines in alphabetical order:" << std::endl;
+                LINES(routes);
+            }
+
+            else if(cmd.at(0) == "LINE")
+            {
+                std::cout << "Line " << cmd.at(1) << " goes through these stops in the order they are listed:" << std::endl;
+                LINE(routes, cmd.at(1));
+            }
+
+            else if(cmd.at(0) == "STOPS")
+            {
+                std::cout << "All stops in alphabetical order:" << std::endl;
+                STOPS(routes);
+            }
+
+            else if(cmd.at(0) == "STOP")
+            {
+                std::cout << "Stop " << cmd.at(1) << " can be found on the following lines:" << std::endl;
+                STOP(routes, cmd.at(1));
+            }
+
+            else if(cmd.at(0) == "DISTANCE")
+            {
+
+            }
+
+            else if(cmd.at(0) == "ADDLINE")
+            {
+
+            }
+
+            else if(cmd.at(0) == "ADDSTOP")
+            {
+
+            }
+
+            else if(cmd.at(0) == "REMOVE")
+            {
+
+            }
+            else
+            {
+                std::cout << "Error: Invalid input." << std::endl;
+            }
+
         }
-
-        else if(cmd.at(0) == "LINE")
-        {
-            std::cout << "Line " << cmd.at(1) << " goes through these stops in the order they are listed:" << std::endl;
-            LINE(routes, cmd.at(1));
-        }
-
-        else if(cmd.at(0) == "STOPS")
-        {
-
-        }
-
-        else if(cmd.at(0) == "STOP")
-        {
-
-        }
-
-        else if(cmd.at(0) == "DISTANCE")
-        {
-
-        }
-
-        else if(cmd.at(0) == "ADDLINE")
-        {
-
-        }
-
-        else if(cmd.at(0) == "ADDSTOP")
-        {
-
-        }
-
-        else if(cmd.at(0) == "REMOVE")
-        {
-
-        }
-        else
-        {
-            std::cout << "Error: Invalid input." << std::endl;
-        }
-
     }
     return EXIT_SUCCESS;
 }
@@ -223,16 +324,13 @@ int map_n_check(std::map<std::string, std::map<std::string, double>>& routes)
                 std::cout << "Error: Stop/line already exists." << std::endl;
                 return EXIT_FAILURE;
             }
-            if(routes.find(row_terms.at(0)) != routes.end()) //pysäkki on ylämapissa?
+            if(routes.find(row_terms.at(0)) != routes.end()) //linja on ylämapissa?
             {
-                if(routes[row_terms.at(0)].find(row_terms.at(1)) != routes[row_terms.at(0)].end())
-                {
-                    continue;
-                }
+                routes[row_terms.at(0)].insert({row_terms.at(1) , distance});
             }
-            else
+            else // uusi linja
             {
-                routes[row_terms.at(0)][row_terms.at(1)] = distance; // tietopari alamappiin
+                routes[row_terms.at(0)][row_terms.at(1)] = distance;
             }
         }
 
@@ -256,7 +354,6 @@ int main()
     map_n_check(routes);
 
     UI(routes);
-
 
     return EXIT_SUCCESS;
 }
