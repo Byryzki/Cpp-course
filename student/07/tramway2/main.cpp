4f126eb8534e8561bb0b793f4b3c1e908f37b2ef
Pyry Laine <pyry.j.laine@tuni.fi>
2021-03-06 16:02:16 +0200

    RASSE -projekti: DISTANCE, ADDLINE, ADDSTOP, REMOVE

--- student/07/tramway2/main.cpp
+++ student/07/tramway2/main.cpp
@@ -81,11 +81,19 @@ std::vector<std::pair<std::string, double>> value_sort(std::map<std::string, dou
 
 void LINE(std::map<std::string, std::map<std::string, double>> routes, std::string line)
 {   
-    std::vector<std::pair<std::string, double>> sorted = value_sort(routes[line]);
-
-    for(auto stop : sorted)
+    if(routes.find(line) != routes.end())
     {
-        std::cout << " - " << stop.first << " : " << stop.second << std::endl;
+         std::cout << "Line " << line << " goes through these stops in the order they are listed:" << std::endl;
+
+        std::vector<std::pair<std::string, double>> sorted = value_sort(routes[line]);
+        for(auto stop : sorted)
+        {
+            std::cout << " - " << stop.first << " : " << stop.second << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Error: Line could not be found." << std::endl;
     }
 }
 
@@ -123,6 +131,7 @@ void STOPS(std::map<std::string, std::map<std::string, double>> routes)
 void STOP(std::map<std::string, std::map<std::string, double>> routes, std::string target)
 {
     std::vector<std::string> lines;
+    bool empty = true;
 
     for(std::map<std::string, std::map<std::string, double>>::iterator line = routes.begin(); line != routes.end(); line++)
     {
@@ -130,12 +139,134 @@ void STOP(std::map<std::string, std::map<std::string, double>> routes, std::stri
         {
             if(stop -> first == target)
             {
-                std::cout << " - " << line -> first << std::endl;
+                empty = false;
+                lines.push_back(line -> first);
             }
+        }
+    }
+    if(empty) // tuntematon pysäkki
+    {
+        std::cout << "Error: Stop could not be found." << std::endl;
+    }
+    else
+    {
+        std::cout << "Stop " << target << " can be found on the following lines:" << std::endl;
+        for(auto& i : lines)
+        {
+            std::cout << " - " << i << std::endl;
         }
     }
 }
 
+void DISTANCE(std::map<std::string, std::map<std::string, double>> routes, std::string line, std::string stop1, std::string stop2)
+{
+    if(routes.find(line) != routes.end()) // linja löytyy
+    {
+        double a = 200 , b = -100;
+        double distance;
+
+        std::vector<std::pair<std::string, double>> sorted = value_sort(routes[line]);
+        for(auto stop : sorted)
+        {
+            if(stop.first == stop1)
+            {
+                b = stop.second;
+            }
+            else if(stop.first == stop2)
+            {
+                a = stop.second;
+            }
+    }
+
+        distance = a - b;
+        if(distance >= 100)
+        {
+            std::cout << "Error: Stop could not be found" << std::endl;
+        }
+        else
+        {
+            std::cout << "Distance between " << stop1 << " and " << stop2 << " is " << distance << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Error: Line could not be found." << std::endl;
+    }
+}
+
+void ADDLINE(std::map<std::string, std::map<std::string, double>>& routes, std::string line)
+{
+    if(routes.find(line) == routes.end())
+    {
+        routes[line] = {}; // uusi tietopari ylämappiin
+        std::cout << "Line was added." << std::endl;
+    }
+    else
+    {
+        std::cout << "Error: Stop/line already exists." << std::endl;
+    }
+}
+
+void ADDSTOP(std::map<std::string, std::map<std::string, double>>& routes, std::string line, std::string new_stop, double distance)
+{
+    bool error = false;
+
+    for(std::map<std::string, std::map<std::string, double>>::iterator lin = routes.begin(); lin != routes.end(); lin++) // pysäkkien tarkastus
+    {
+        for(std::map<std::string, double>::iterator stop = routes[lin -> first].begin(); stop != routes[lin -> first].end(); stop++)
+        {
+            if(stop -> first == new_stop)
+            {
+                error = true;
+            }
+            else if(stop -> second == distance)
+            {
+                error = true;
+            }
+        }
+    }
+
+    if(error == false) // ei päällekkäisyyksiä alamapissa
+    {
+        if(routes.find(line) != routes.end()) // linja on ylämapissa
+        {
+            routes[line][new_stop] = distance; // uusi tietopari alamappiin
+            std::cout << "Stop was added." << std::endl;
+        }
+        else
+        {
+            std::cout << "Error: Stop/line already exists." << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Error: Stop/line already exists." << std::endl;
+    }
+}
+
+void REMOVE(std::map<std::string, std::map<std::string, double>>& routes, std::string dstop)
+{
+    bool empty = true;
+    for(std::map<std::string, std::map<std::string, double>>::iterator line = routes.begin(); line != routes.end(); line++) // käydään läpi linjojen pysäkit
+    {
+        const auto hit = routes[line -> first].find(dstop);
+        if(hit != routes[line -> first].end())
+        {
+            routes[line -> first].erase(hit);
+            empty = false;
+        }
+    }
+
+    if(empty)
+    {
+        std::cout << "Error: Stop could not be found." << std::endl;
+    }
+    else
+    {
+        std::cout << "Stop was removed from all lines." << std::endl;
+    }
+
+}
 /* Jos jaksaminen riittää, korjaa lainausmerkkejä varten
 std::vector<std::string> command_repair(std::string command, char del)
 {
@@ -185,46 +316,100 @@ int UI(std::map<std::string, std::map<std::string, double>> routes)
             /* cmd.at(0): komento, cmd.at(1): linja tai pysäkki yms. */
             if(cmd.at(0) == "LINES")
             {
-                std::cout << "All tramlines in alphabetical order:" << std::endl;
-                LINES(routes);
+                if(cmd.size() == 1)
+                {
+                    std::cout << "All tramlines in alphabetical order:" << std::endl;
+                    LINES(routes);
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "LINE")
             {
-                std::cout << "Line " << cmd.at(1) << " goes through these stops in the order they are listed:" << std::endl;
-                LINE(routes, cmd.at(1));
+                if(cmd.size() == 2)
+                {
+                    LINE(routes, cmd.at(1));
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "STOPS")
             {
-                std::cout << "All stops in alphabetical order:" << std::endl;
-                STOPS(routes);
+                if(cmd.size() == 1)
+                {
+                    std::cout << "All stops in alphabetical order:" << std::endl;
+                    STOPS(routes);
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "STOP")
             {
-                std::cout << "Stop " << cmd.at(1) << " can be found on the following lines:" << std::endl;
-                STOP(routes, cmd.at(1));
+                if(cmd.size() == 2)
+                {
+                    STOP(routes, cmd.at(1));
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "DISTANCE")
             {
-
+                if(cmd.size() == 4)
+                {
+                    DISTANCE(routes, cmd.at(1), cmd.at(2), cmd.at(3));
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "ADDLINE")
             {
-
+                if(cmd.size() == 2)
+                {
+                    ADDLINE(routes, cmd.at(1));
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "ADDSTOP")
             {
-
+                if(cmd.size() == 4)
+                {
+                    ADDSTOP(routes, cmd.at(1), cmd.at(2), stod(cmd.at(3)));
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
 
             else if(cmd.at(0) == "REMOVE")
             {
-
+                if(cmd.size() == 2)
+                {
+                    REMOVE(routes, cmd.at(1));
+                }
+                else
+                {
+                    std::cout << "Error: Invalid input." << std::endl;
+                }
             }
             else
             {
